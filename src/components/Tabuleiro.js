d6b9785d7b501b3efb9548e64ced6c0a6e867c7c
José Gomes <106411184+gomes-tkd@users.noreply.github.com>
2022-12-30 15:08:01 -0300

    tabuleiro modificado

--- src/components/Tabuleiro.js
+++ src/components/Tabuleiro.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from '../assets/css/tabuleiro.module.css';
 
 const Tabuleiro = ({
@@ -8,198 +8,204 @@ const Tabuleiro = ({
                        placarEmpates, setPlacarEmpates,
                        placarTotalPartidas, setPlacarTotalPartidas}) => {
 
-    function handleJogadorAtual(e) {
+
+    const [board, setBoard] = useState([
+        [" ", " ", " "],
+        [" ", " ", " "],
+        [" ", " ", " "]
+    ]);
+
+    function handleJogadorAtual(e, rowIndex, cellIndex) {
+        let copiaBoard = [...board];
+
         if (turno < 9) {
             if (turno % 2 === 0) {
-                e.currentTarget.innerHTML = "X";
-                setJogadorAtual("O");
+                if (copiaBoard[rowIndex][cellIndex] === " ") {
+                    copiaBoard[rowIndex][cellIndex] = "X";
+                    setJogadorAtual("O");
+                } else {
+                    alert("Casa já ocupada!");
+                    setJogadorAtual("X")
+                    turno--;
+                }
+
             } else {
-                e.currentTarget.innerHTML = "O";
+                if (copiaBoard[rowIndex][cellIndex] === " ") {
+                    copiaBoard[rowIndex][cellIndex] = "O";
+                    setJogadorAtual("X");
+                } else {
+                    alert("Casa já ocupada!");
+                    setJogadorAtual("O");
+                    turno--;
+                }
+                copiaBoard[rowIndex][cellIndex] = "O";
                 setJogadorAtual("X");
             }
-
+            setBoard(copiaBoard);
             setTurno(turno + 1);
         } else {
             alert("VELHA! Número máximo de jogadas permitidas atingido!");
-            const td = document.querySelectorAll("td");
-            td.forEach(cell => cell.innerHTML = " ");
             setPlacarEmpates(placarEmpates + 1);
-            setPlacarTotalPartidas(placarTotalPartidas + 1);
-            setTurno(1);
+            handleClearBoard();
         }
         handleCondicoesVitoria();
     }
 
     function handleCondicoesVitoria() {
-        const tabuleiro = document.querySelector("tbody").rows;
-        const td = document.querySelectorAll("td");
-
-        if (tabuleiro[0].cells[0].innerHTML === "X" && tabuleiro[0].cells[1].innerHTML === "X" && tabuleiro[0].cells[2].innerHTML === "X" ) {
+        if (board[0][0] === "X" && board[0][1] === "X" && board[0][2] === "X" ) {
             alert("Jogador X venceu!");
             setPlacarJogadorX(placarJogadorX + 1);
-            td.forEach(cell => cell.innerHTML = " ");
-            setTurno(1);
-            setPlacarTotalPartidas(placarTotalPartidas + 1);
-
-        } else if (tabuleiro[1].cells[0].innerHTML === "X" && tabuleiro[1].cells[1].innerHTML === "X" && tabuleiro[1].cells[2].innerHTML === "X" ) {
+            handleClearBoard();
+        } else if (board[1][0] === "X" && board[1][1] === "X" && board[1][2] === "X" ) {
             alert("Jogador X venceu!");
             setPlacarJogadorX(placarJogadorX + 1);
-            td.forEach(cell => cell.innerHTML = " ");
-            setTurno(1);
-            setPlacarTotalPartidas(placarTotalPartidas + 1);
-
-        } else if (tabuleiro[2].cells[0].innerHTML === "X" && tabuleiro[2].cells[1].innerHTML === "X" && tabuleiro[2].cells[2].innerHTML === "X" ) {
+            handleClearBoard();
+        } else if (board[2][0] === "X" && board[2][1] === "X" && board[2][2] === "X" ) {
             alert("Jogador X venceu!");
             setPlacarJogadorX(placarJogadorX + 1);
-            td.forEach(cell => cell.innerHTML = " ");
-            setTurno(1);
-            setPlacarTotalPartidas(placarTotalPartidas + 1);
-
-        } else if (tabuleiro[0].cells[0].innerHTML === "X" && tabuleiro[1].cells[0].innerHTML === "X" && tabuleiro[2].cells[0].innerHTML === "X" ) {
+            handleClearBoard();
+        } else if (board[2][0] === "X" && board[2][1] === "X" && board[2][2] === "X" ) {
             alert("Jogador X venceu!");
             setPlacarJogadorX(placarJogadorX + 1);
-            td.forEach(cell => cell.innerHTML = " ");
-            setTurno(1);
-            setPlacarTotalPartidas(placarTotalPartidas + 1);
-
-        } else if (tabuleiro[0].cells[1].innerHTML === "X" && tabuleiro[1].cells[1].innerHTML === "X" && tabuleiro[2].cells[1].innerHTML === "X" ) {
+            handleClearBoard();
+        } else if (board[0][0] === "X" && board[1][0] === "X" && board[2][0] === "X" ) {
             alert("Jogador X venceu!");
             setPlacarJogadorX(placarJogadorX + 1);
-            td.forEach(cell => cell.innerHTML = " ");
-            setTurno(1);
-            setPlacarTotalPartidas(placarTotalPartidas + 1);
-
-        } else if (tabuleiro[0].cells[2].innerHTML === "X" && tabuleiro[1].cells[2].innerHTML === "X" && tabuleiro[2].cells[2].innerHTML === "X" ) {
+            handleClearBoard();
+        } else if (board[0][1] === "X" && board[1][1] === "X" && board[2][1] === "X" ) {
             alert("Jogador X venceu!");
             setPlacarJogadorX(placarJogadorX + 1);
-            td.forEach(cell => cell.innerHTML = " ");
-            setTurno(1);
-            setPlacarTotalPartidas(placarTotalPartidas + 1);
-
-        } else if (tabuleiro[0].cells[0].innerHTML === "X" && tabuleiro[1].cells[1].innerHTML === "X" && tabuleiro[2].cells[2].innerHTML === "X" ) {
+            handleClearBoard();
+        } else if (board[0][2] === "X" && board[1][2] === "X" && board[2][2] === "X" ) {
             alert("Jogador X venceu!");
             setPlacarJogadorX(placarJogadorX + 1);
-            td.forEach(cell => cell.innerHTML = " ");
-            setTurno(1);
-            setPlacarTotalPartidas(placarTotalPartidas + 1);
-
-        } else if (tabuleiro[0].cells[2].innerHTML === "X" && tabuleiro[1].cells[1].innerHTML === "X" && tabuleiro[2].cells[0].innerHTML === "X" ) {
+            handleClearBoard();
+        } else if (board[0][0] === "X" && board[1][1] === "X" && board[2][2] === "X" ) {
             alert("Jogador X venceu!");
             setPlacarJogadorX(placarJogadorX + 1);
-            td.forEach(cell => cell.innerHTML = " ");
-            setTurno(1);
-            setPlacarTotalPartidas(placarTotalPartidas + 1);
-
+            handleClearBoard();
+        } else if (board[0][2] === "X" && board[1][1] === "X" && board[2][0] === "X" ) {
+            alert("Jogador X venceu!");
+            setPlacarJogadorX(placarJogadorX + 1);
+            handleClearBoard();
         }
 
-        else if (tabuleiro[0].cells[0].innerHTML === "O" && tabuleiro[0].cells[1].innerHTML === "O" && tabuleiro[0].cells[2].innerHTML === "O" ) {
+        else if (board[0][0] === "O" && board[0][1] === "O" && board[0][2] === "O" ) {
             alert("Jogador O venceu!");
             setPlacarJogadorO(placarJogadorO + 1);
-            td.forEach(cell => cell.innerHTML = " ");
-            setTurno(1);
-            setPlacarTotalPartidas(placarTotalPartidas + 1);
-
-
-        } else if (tabuleiro[1].cells[0].innerHTML === "O" && tabuleiro[1].cells[1].innerHTML === "O" && tabuleiro[1].cells[2].innerHTML === "O" ) {
+            handleClearBoard();
+        } else if (board[1][0] === "O" && board[1][1] === "O" && board[1][2] === "O" ) {
             alert("Jogador O venceu!");
             setPlacarJogadorO(placarJogadorO + 1);
-            td.forEach(cell => cell.innerHTML = " ");
-            setTurno(1);
-            setPlacarTotalPartidas(placarTotalPartidas + 1);
-
-        } else if (tabuleiro[2].cells[0].innerHTML === "O" && tabuleiro[2].cells[1].innerHTML === "O" && tabuleiro[2].cells[2].innerHTML === "O" ) {
+            handleClearBoard();
+        } else if (board[2][0] === "O" && board[2][1] === "O" && board[2][2] === "O" ) {
             alert("Jogador O venceu!");
             setPlacarJogadorO(placarJogadorO + 1);
-            td.forEach(cell => cell.innerHTML = " ");
-            setTurno(1);
-            setPlacarTotalPartidas(placarTotalPartidas + 1);
-
-        } else if (tabuleiro[0].cells[0].innerHTML === "O" && tabuleiro[1].cells[0].innerHTML === "O" && tabuleiro[2].cells[0].innerHTML === "O" ) {
+            handleClearBoard();
+        } else if (board[0][0] === "O" && board[1][0] === "O" && board[2][0] === "O" ) {
             alert("Jogador O venceu!");
             setPlacarJogadorO(placarJogadorO + 1);
-            td.forEach(cell => cell.innerHTML = " ");
-            setTurno(1);
-            setPlacarTotalPartidas(placarTotalPartidas + 1);
-
-        } else if (tabuleiro[0].cells[1].innerHTML === "O" && tabuleiro[1].cells[1].innerHTML === "O" && tabuleiro[2].cells[1].innerHTML === "O" ) {
+            handleClearBoard();
+        } else if (board[0][1] === "O" && board[1][1] === "O" && board[2][1] === "O" ) {
             alert("Jogador O venceu!");
             setPlacarJogadorO(placarJogadorO + 1);
-            td.forEach(cell => cell.innerHTML = " ");
-            setTurno(1);
-            setPlacarTotalPartidas(placarTotalPartidas + 1);
-
-        } else if (tabuleiro[0].cells[2].innerHTML === "O" && tabuleiro[1].cells[2].innerHTML === "O" && tabuleiro[2].cells[2].innerHTML === "O") {
+            handleClearBoard();
+        } else if (board[0][2] === "O" && board[1][2] === "O" && board[2][2] === "O" ) {
             alert("Jogador O venceu!");
             setPlacarJogadorO(placarJogadorO + 1);
-            td.forEach(cell => cell.innerHTML = " ");
-            setTurno(1);
-            setPlacarTotalPartidas(placarTotalPartidas + 1);
-
-        } else if (tabuleiro[0].cells[0].innerHTML === "O" && tabuleiro[1].cells[1].innerHTML === "O" && tabuleiro[2].cells[2].innerHTML === "O" ) {
+            handleClearBoard();
+        } else if (board[0][0] === "O" && board[1][1] === "O" && board[2][2] === "O" ) {
             alert("Jogador O venceu!");
             setPlacarJogadorO(placarJogadorO + 1);
-            td.forEach(cell => cell.innerHTML = " ");
-            setTurno(1);
-            setPlacarTotalPartidas(placarTotalPartidas + 1);
-
-        } else if (tabuleiro[0].cells[2].innerHTML === "O" && tabuleiro[1].cells[1].innerHTML === "O" && tabuleiro[2].cells[0].innerHTML === "O" ) {
+            handleClearBoard();
+        } else if (board[0][2] === "O" && board[1][1] === "O" && board[2][0] === "O" ) {
             alert("Jogador O venceu!");
             setPlacarJogadorO(placarJogadorO + 1);
-            td.forEach(cell => cell.innerHTML = " ");
-            setTurno(1);
-            setPlacarTotalPartidas(placarTotalPartidas + 1);
-
+            handleClearBoard();
         }
     }
 
+    function handleClearBoard() {
+        setBoard([
+            [" ", " ", " "],
+            [" ", " ", " "],
+            [" ", " ", " "]
+        ]);
+        setTurno(1);
+        setPlacarTotalPartidas(placarTotalPartidas + 1);
+        setJogadorAtual("O");
+    }
+
     return (
-        <table className={styles.container}>
-            <tbody>
-            <tr>
-                <td
-                    onClick={handleJogadorAtual}
-                    className={styles.horizontal}
-                >
-                </td>
-                <td
-                    onClick={handleJogadorAtual}
-                    className={`${styles.vertical} ${styles.horizontal}`}
-                >
-                </td>
-                <td
-                    onClick={handleJogadorAtual}
-                    className={styles.horizontal}
-                >
-                </td>
-            </tr>
-            <tr>
-                <td
-                    onClick={handleJogadorAtual}
-                    className={styles.horizontal}
-                >
-                </td>
-                <td
-                    onClick={handleJogadorAtual}
-                    className={`${styles.vertical} ${styles.horizontal}`}
-                >
-                </td>
-                <td
-                    onClick={handleJogadorAtual}
-                    className={styles.horizontal}
-                >
-                </td>
-            </tr>
-            <tr>
-                <td onClick={handleJogadorAtual}></td>
-                <td
-                    onClick={handleJogadorAtual}
-                    className={styles.vertical}
-                >
-                </td>
-                <td onClick={handleJogadorAtual}></td>
-            </tr>
-            </tbody>
-        </table>
+        <>
+            {/*
+            <table className={styles.container}>
+                <tbody>
+                <tr>
+                    <td
+                        onClick={handleJogadorAtual}
+                        className={styles.horizontal}
+                    >
+                    </td>
+                    <td
+                        onClick={handleJogadorAtual}
+                        className={`${styles.vertical} ${styles.horizontal}`}
+                    >
+                    </td>
+                    <td
+                        onClick={handleJogadorAtual}
+                        className={styles.horizontal}
+                    >
+                    </td>
+                </tr>
+                <tr>
+                    <td
+                        onClick={handleJogadorAtual}
+                        className={styles.horizontal}
+                    >
+                    </td>
+                    <td
+                        onClick={handleJogadorAtual}
+                        className={`${styles.vertical} ${styles.horizontal}`}
+                    >
+                    </td>
+                    <td
+                        onClick={handleJogadorAtual}
+                        className={styles.horizontal}
+                    >
+                    </td>
+                </tr>
+                <tr>
+                    <td onClick={handleJogadorAtual}></td>
+                    <td
+                        onClick={handleJogadorAtual}
+                        className={styles.vertical}
+                    >
+                    </td>
+                    <td onClick={handleJogadorAtual}></td>
+                </tr>
+                </tbody>
+            </table>*/}
+            <table className={styles.container}>
+                <tbody>
+                {
+                    board.map((row, rowIndex) => (
+                        <tr key={rowIndex}>
+                            {row.map((cell, cellIndex) => (
+                                <td
+                                    className={styles.square}
+                                    onClick={() => handleJogadorAtual(cell, rowIndex, cellIndex)}
+                                    key={cellIndex}
+                                >
+                                    {cell}
+                                </td>))}
+                        </tr>
+                    ))
+                }
+                </tbody>
+            </table>
+        </>
+
     );
 };
 
